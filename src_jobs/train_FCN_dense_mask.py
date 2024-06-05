30f12e8fe68039b37f5293b65e444a86983e3c62
hvonhue <140177315+hvonhue@users.noreply.github.com>
2024-06-05 01:37:44 +0000

    train files for FCN dense model

--- src_jobs/train_FCN_dense_mask.py
+++ src_jobs/train_FCN_dense_mask.py
@@ -1,5 +1,5 @@
 """
-python train_FCN_dense:slidingWindow.py --input-path ../data/processed --val-path ../data/val_files/val_SW_noCiECGT512.pkl --output-path ../data/output
+python train_FCN_dense_mask.py --input-path ../data/processed --val-path ../data/val_files/val_mask_noCiECGT512.pkl --output-path ../data/output
 """
 import pickle
 import warnings
@@ -11,16 +11,13 @@ import mlflow
 import numpy as np
 import torch
 import typer
-from artifact import Saw_centered
-from data import CachedArtifactDataset, CenteredArtifactDataset
+from artifact import Saw
+from data import CachedArtifactDataset, RealisticArtifactDataset
+from mask_detector import WindowLinearDetector
+from modeling import DelayedEarlyStopping
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import (
-    EarlyStopping,
-    LearningRateMonitor,
-    ModelCheckpoint,
-)
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import MLFlowLogger
-from sliding_window_detector import SlidingWindowLinearDetector
 from torch.utils.data import DataLoader
 from utilities import parameters_k
 
@@ -30,57 +27,54 @@ warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
 # # width of window
 width = 512
-convolution_features = [256, 128, 64, 32]
-convolution_width = [5, 9, 17, 33]
+convolution_features = [256, 128, 64, 32]  # [256, 128, 64, 32] adapted FCN # [128, 256, 128] FCN
+convolution_width = [5, 9, 17, 33]  # [5, 9, 17, 33] adapted FCN # [8, 5, 3] FCN
 convolution_dropout = 0.0
-pooling_kernel = 4
-linear_layers = [32, 32]
-loss = "label"  # "mask" for mask detector, "label for sliding window"
-loss_boost_fp = 0.5
-artifact = Saw_centered()
+linear_layers = [64, 64]
+loss = "mask"  # "mask" for mask detector, "label for sliding window"
+loss_boost_fp = 0.2
+artifact = Saw()
 batch_size = 32  # 'values': [32, 64, 128]
-warmup = 15000
+warmup = 30
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # model
-cnn_dense_model = SlidingWindowLinearDetector(
+model = WindowLinearDetector(
     window=width,
     convolution_features=convolution_features,
     convolution_width=convolution_width,
     convolution_dropout=0.0,
-    pooling_kernel=pooling_kernel,
     linear_layers=linear_layers,
     loss=loss,
     loss_boost_fp=loss_boost_fp,
     warmup=warmup,
 )
-
-model = cnn_dense_model
 model_name = f"{model.__class__.__name__}_{loss}_{parameters_k(model)}_{datetime.now().strftime('%d-%m-%Y_%H:%M:%S')}"
 run_name = model_name
 
 train_datasets = [
-    # 'CinCECGTorso',
-    "australian_electricity_demand_dataset",
-    "electricity_hourly_dataset",
-    "electricity_load_diagrams",
-    "HouseholdPowerConsumption1",
-    "london_smart_meters_dataset_without_missing_values",
-    "solar_10_minutes_dataset",
-    "wind_farms_minutely_dataset_without_missing_values",
-    'ACSF1',
-    'HouseTwenty',
-    'Mallat',
-    'MixedShapesRegularTrain',
-    'Phoneme',
-    'PigArtPressure',
-    'PigCVP',
-    'Rock',
-    'SemgHandGenderCh2',
-    'mitbih',
-    'ptbdb',
-    'ETTh',
-    'ETTm'
+    # 'CinCECGTorso', # do not train on this dataset for validation purposes
+    "ETTm",  # 1
+    "ETTh",  # 2
+    "electricity_load_diagrams",  # 3
+    "australian_electricity_demand_dataset",  # 4
+    "Phoneme",  # 5
+    "electricity_hourly_dataset",  # 6
+    "HouseholdPowerConsumption1",  # 7
+    "london_smart_meters_dataset_without_missing_values",  # 8
+    "SemgHandGenderCh2",  # 9
+    "PigCVP",  # 10
+    "HouseTwenty",  # 11
+    "wind_farms_minutely_dataset_without_missing_values",  # 12
+    "ptbdb",  # 13
+    "mitbih",  # 14
+    "PigArtPressure",  # 15
+    "solar_10_minutes_dataset",  # 16
+    "Mallat",  # 17
+    "MixedShapesRegularTrain",  # 18
+    "Rock",  # 19
+    "ACSF1",  # 20
 ]
 print(model_name)
 
@@ -117,28 +111,28 @@ def main(
         input_path.resolve().is_dir()
     ), f"Provided 'input_path' directory ({input_path}) doesn't exist!"
 
-
     # initialize logger
     logger = MLFlowLogger(
         log_model="all",
         run_name=model_name,
-        experiment_name="artifactory_CNN_Pool_detector",
+        experiment_name="artifactory_Dense_detector",
         tracking_uri=mlflow.get_tracking_uri(),
     )
 
     # validation
     val_file = Path(f"{val_path}")
-    # validation
+
     if not val_file.exists():
-        val_data, val_weights = load_series(train_datasets, "ALL")
-        val_gen = CenteredArtifactDataset(val_data,
-                                width=width,
-                                padding=64,
-                                artifact=artifact,
-                                weight=val_weights)
-        val = CachedArtifactDataset.generate(val_gen,
-                                            n=2048,
-                                            to=val_file)
+        f"'val_file' at provided 'val_path' directory ({val_path}) doesn't exist! Creating validation file..."
+        val_data, val_weights = load_series(train_datasets, "VAL", str(input_path))
+        val_gen = RealisticArtifactDataset(
+            val_data,
+            width=width,
+            padding=64,
+            artifact=artifact,
+            weight=val_weights,
+        )
+        val = CachedArtifactDataset.generate(val_gen, n=2048, to=val_file)
     else:
         val = CachedArtifactDataset(file=val_file)
     val_loader = DataLoader(val, batch_size=batch_size)
@@ -146,7 +140,7 @@ def main(
     # train
     train_data, train_weights = load_series(train_datasets, "TRAIN", str(input_path))
     print("Dataset")
-    train_dataset = CenteredArtifactDataset(
+    train_dataset = RealisticArtifactDataset(
         train_data,
         width=width,
         padding=64,
@@ -161,26 +155,28 @@ def main(
 
     # initialize callbacks
     checkpointcallback = ModelCheckpoint(
-        dirpath=output_path, monitor="val_fbeta", mode="max", save_top_k=1
+        dirpath=f"{output_path}/{model_name}",
+        monitor="val_fbeta",
+        mode="max",
+        save_top_k=1,
     )
     lr_monitor = LearningRateMonitor(logging_interval="step")
-    early_stop_callback = EarlyStopping(
-        monitor="val_fbeta", min_delta=0.0, patience=20, verbose=True, mode="max"
+    early_stop_callback = DelayedEarlyStopping(
+        warmupES=10000, monitor="validation", min_delta=0.005, patience=10
     )
-
     # initialize trainer
     trainer = Trainer(
         logger=logger,
         max_steps=50000,
         val_check_interval=500,
-        callbacks=[checkpointcallback, lr_monitor, early_stop_callback],
+        callbacks=[checkpointcallback, early_stop_callback, lr_monitor],
     )
     print("Initialized trainer.")
 
     # Auto log all MLflow entities
     mlflow.pytorch.autolog(log_every_n_step=500)
 
-    print("Starting training.")
+    print("Fit")
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     print("Training completed.")
