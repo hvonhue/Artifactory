cb3bf1dd26c3244e7b9820b59ea7a357496a5750
hvonhue <140177315+hvonhue@users.noreply.github.com>
2024-07-03 21:49:55 +0000

    test

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -6,7 +6,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
